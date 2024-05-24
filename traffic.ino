9ae13b5802582ac19d8c02554820904610373c9d
Karan K.C <60384471+kirajkc@users.noreply.github.com>
2024-05-24 15:24:05 +0900

    Add files via upload

--- traffic.ino
+++ traffic.ino
@@ -5,26 +5,25 @@
 void setup(){
   //Init Serial USB
   Serial.begin(9600);
-  Serial.println(F("Initialize System"));
- 	pinMode(channel1A, OUTPUT);
- 	pinMode(channel1B, OUTPUT); 
- 	pinMode(channel1C, OUTPUT); 
+   pinMode(channel1A, OUTPUT);
+  pinMode(channel1B, OUTPUT); 
+  pinMode(channel1C, OUTPUT); 
 }
 
 
 void loop(){
   digitalWrite(channel1A, 1);
-  Serial.println("red");
+  Serial.println("yellow");
   delay(10000);
   digitalWrite(channel1A,0);
 
   digitalWrite(channel1B, 1);
-  Serial.println("yellow");
+  Serial.println("green");
   delay(10000);
   digitalWrite(channel1B,0);
 
   digitalWrite(channel1C, 1);
-  Serial.println("green");
+  Serial.println("red");
   delay(10000);
   digitalWrite(channel1C,0);
   Serial.flush();
